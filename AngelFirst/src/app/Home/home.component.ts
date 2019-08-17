d645f0160db3ec0f6c765f53d226264c3f68ce9e
ARUN <arun26cs@gmail.com>
2019-08-18 00:01:27 +0530

    Created home and aboute page. Added Routing along with SPA implementation.

---
+++ AngelFirst/src/app/Home/home.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core';
+
+
+@Component({
+    templateUrl: './home.component.html'
+})
+
+export class HomeComponent {
+    Message ='This is the home page content'
+}
---
+++ AngelFirst/src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core';
+
+
+@Component({
+    templateUrl: './about.component.html'
+})
+
+export class AboutComponent {
+    Message = 'This application is created to add tasks and amnage them based on its schedules.'
+}
